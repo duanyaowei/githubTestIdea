3c22685132b062c5d90bbfac9c849a4d9bd338f6
duan_glo <duan_glo@qq>
2021-06-08 20:52:28 +0800

    push commit 在test类中创建了一个push方法

--- src/main/java/com/baidu/springsecurity/test/Test.java
+++ src/main/java/com/baidu/springsecurity/test/Test.java
@@ -11,4 +11,8 @@ public class Test {
     public void abc(){
         System.out.println("这是一个master方法..");
     }
+
+    public void push(){
+        System.out.println("这是测试push到远程仓库是否成功的方法...");
+    }
 }
